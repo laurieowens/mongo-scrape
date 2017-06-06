238038ffdb2973c96aaa4c23948ddd0340aa1fda
Jake Owens <Jowens@Jakes-Air-3.attlocal.net>
2017-06-06 17:51:42 -0400

    wip4

--- server.js
+++ server.js
@@ -6,8 +6,8 @@ var bodyParser = require("body-parser");
 var logger = require("morgan");
 var mongoose = require("mongoose");
 // Requiring our Note and Article models
-var Note = require("./models/Note.js");
-var Article = require("./models/Article.js");
+var Note = require("./models/note.js");
+var Article = require("./models/article.js");
 // Our scraping tools
 var request = require("request");
 var cheerio = require("cheerio");
@@ -90,7 +90,7 @@ app.get("/scrape", function(req, res) {
   });
   // Tell the browser that we finished scraping the text
   res.send("Scrape Complete");
-  
+
 });
 
 
