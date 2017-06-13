e01a71f82382136fa8426c8ed8c6cacdeba6c7ae
Jake Owens <Jowens@Jakes-Air-3.attlocal.net>
2017-06-12 20:39:05 -0400

    corrections

--- server.js
+++ server.js
@@ -6,8 +6,8 @@ var bodyParser = require("body-parser");
 var logger = require("morgan");
 var mongoose = require("mongoose");
 // Requiring our Note and Article models
-var Note = require("./models/note.js");
-var Article = require("./models/article.js");
+var Note = require("./models/Note.js");
+var Article = require("./models/Article.js");
 // Our scraping tools
 var request = require("request");
 var cheerio = require("cheerio");
